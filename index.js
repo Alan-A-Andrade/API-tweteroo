2f291ee6097280e0ab15ecc9122ee78aae481743
lostzilla <alanaa92@gmail.com>
2022-01-21 10:26:45 -0300

    sign-up implemented

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
+
+const users = []
 
 app.get("/hello", (req, res) => {
-  res.send('**Meu primeiro servidor, yay!**');
+  res.send(users);
 });
 
-app.listen(5001);
+app.post("/sign-up", (req, res) => {
+
+  const user = req.body;
+  users.push(user)
+
+  res.send("OK");
+});
+
+app.listen(5000);
